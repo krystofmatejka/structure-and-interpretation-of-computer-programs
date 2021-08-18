301b35ba5a5146816461f94eadec78d6bf1aaa5e
Kryštof Matějka <kx.matejka@gmail.com>
2021-08-18 20:57:44 +0200

    add iterative factorial

--- src/1-building-abstractions-with-procedures/factorial.js
+++ src/1-building-abstractions-with-procedures/factorial.js
@@ -1,4 +1,4 @@
-import {when, eq, multiply, subtract} from '../../lib'
+import {when, eq, multiply, subtract, gt, sum} from '../../lib'
 
 const factorial = (x) => when(
     eq(x, 1),
@@ -13,4 +13,23 @@ test('factorial', () => {
     expect(factorial(1)).toBe(1)
     expect(factorial(5)).toBe(120)
     expect(factorial(10)).toBe(3_628_800)
+})
+
+const factorial2 = (x) => {
+    const iteration = (product, counter) => when(
+        gt(counter, x),
+        () => product,
+        () => iteration(
+            multiply(counter, product),
+            sum(counter, 1)
+        )
+    )
+
+    return iteration(1, 1)
+}
+
+test('factorial2', () => {
+    expect(factorial2(1)).toBe(1)
+    expect(factorial2(5)).toBe(120)
+    expect(factorial2(10)).toBe(3_628_800)
 })
